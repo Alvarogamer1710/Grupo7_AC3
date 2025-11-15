db74c25592f98c1a45a399122256826e6be220e4
diablometal <Juan.labajolorenzana@gmail.com>
2025-11-15 16:54:38 +0100

    Implementar la clase GestorMusica y sus métodos de búsqueda en main.cpp

--- main.cpp
+++ main.cpp
@@ -1,12 +1,31 @@
 #include <iostream>
-# include "biblioteca_musica.cpp";
-#include "Cancion.cpp";
+
+#include "GestorMusica.cpp";
 
 
 int main() {
-    biblioteca_musica<int> biblioteca;
-    biblioteca.push_back(10);
-    biblioteca.push_back(20);
-    biblioteca.push_back(30);
+    GestorMusica gestor;
+    Cancion c1("Imagine", "Imagine", "John Lennon", "Rock");
+    Cancion c2("Billie Jean", "Thriller", "Michael Jackson", "Pop");
+    Cancion c3("Smells Like Teen Spirit", "Nevermind", "Nirvana", "Grunge");
+    gestor.agregarCancion(c1);
+    gestor.agregarCancion(c2);
+    gestor.agregarCancion(c3);
+    std::cout << "Buscando por titulo 'Imagine':\n";
+    gestor.buscarPorTitulo("Imagine");
+    std::cout << "\nBuscando por album 'Thriller':\n";
+    gestor.buscarPorAlbum("Thriller");
+    std::cout << "\nBuscando por grupo 'Nirvana':\n";
+    gestor.buscarPorGrupo("Nirvana");
+    std::cout << "\nBuscando por genero 'Pop':\n";
+    gestor.buscarPorGenero("Pop");
+
+    std::cout << "\nSin busquedas:\n";
+    gestor.buscarPorGenero("Unknown Genre");
+    gestor.buscarPorTitulo("Unknown Song");
+    gestor.buscarPorAlbum("Unknown Song");
+    gestor.buscarPorGrupo("Unknown Group");
+
+
 
 }
