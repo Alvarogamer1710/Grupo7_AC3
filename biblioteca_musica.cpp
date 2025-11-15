4cb061d66d2f02d7d02ed3763dca821349bd9d77
diablometal <Juan.labajolorenzana@gmail.com>
2025-11-15 16:26:56 +0100

    Clase biblioteca V3

--- biblioteca_musica.cpp
+++ biblioteca_musica.cpp
@@ -3,52 +3,23 @@
 //
 
 #include <iostream>
-#include <string>
-#include <utility>
-#include <stdexcept>
-#include <cstddef>
 
-class Cancion
-{
-public:
-    std::string title;
-    std::string album;
-    std::string artist;
-    std::string genre;
-
-    Cancion() = default;
-    Cancion(std::string t, std::string a, std::string ar, std::string g)
-        : title(std::move(t)), album(std::move(a)), artist(std::move(ar)), genre(std::move(g)) {}
-
-    void print() const
-    {
-        std::cout << "Song: " << title
-                  << " | Album: " << album
-                  << " | Artist: " << artist
-                  << " | Genre: " << genre << '\n';
-    }
-
-    bool operator==(const Cancion& other) const
-    {
-        return title == other.title && album == other.album && artist == other.artist && genre == other.genre;
-    }
-};
-
+template <typename T>
 class biblioteca_musica {
 private:
-    Cancion* data_;
+    T* data_;
     size_t size_;
     size_t capacity_;
 
     void ampliacionSiNecesario() {
         if (size_ >= capacity_) {
             size_t nuevaCapacidad = (capacity_ == 0) ? 1 : capacity_ * 2;
-            Cancion* nuevasCanciones = new Cancion[nuevaCapacidad];
+            T* nuevas = new T[nuevaCapacidad];
             for (size_t i = 0; i < size_; ++i) {
-                nuevasCanciones[i] = data_[i];
+                nuevas[i] = data_[i];
             }
             delete[] data_;
-            data_ = nuevasCanciones;
+            data_ = nuevas;
             capacity_ = nuevaCapacidad;
         }
     }
@@ -56,13 +27,13 @@ public:
     biblioteca_musica() : data_(nullptr), size_(0), capacity_(0) {}
     ~biblioteca_musica() { delete[] data_; }
 
-    void push_back(const Cancion& cancion) {
+    void push_back(const T& valor) {
         ampliacionSiNecesario();
-        data_[size_++] = cancion;
+        data_[size_++] = valor;
     }
-    bool quitarPrimero(const Cancion& cancion) {
+    bool quitarPrimero(const T& valor) {
         for (size_t i = 0; i < size_; ++i) {
-            if (data_[i] == cancion) {
+            if (data_[i] == valor) {
                 for (size_t j = i; j + 1 < size_; ++j) data_[j] = data_[j + 1];
                 --size_;
                 return true;
@@ -72,11 +43,11 @@ public:
     }
     size_t size() const { return size_; }
 
-    Cancion& operator[](size_t index) {
+    T& operator[](size_t index) {
         if (index >= size_) throw std::out_of_range("index out of range");
         return data_[index];
     }
-    const Cancion& operator[](size_t index) const {
+    const T& operator[](size_t index) const {
         if (index >= size_) throw std::out_of_range("index out of range");
         return data_[index];
     }
